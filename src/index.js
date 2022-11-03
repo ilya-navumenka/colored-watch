08ef16f4689f0ba12c490fc614b71e79d165ed54
Ilya Navumenka <navumenkawork@gmail.com>
2022-11-03 11:45:38 -0700

    init: add colored-watch project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    setInterval(hexoClock, 1000);
+}
+
+let clock = document.querySelector('.clock');
+let color = document.querySelector('.color');
+
+let hexoClock = () => {
+    let time = new Date();
+    let h = (time.getHours() % 12).toString();
+    let m = time.getMinutes().toString();
+    let s = time.getSeconds().toString();
+
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (s.length < 2) {
+        s = '0' + s;
+    }
+
+    let clockString = h + ':' + m + ':' + s;
+    let colorString = '#' + h + m + s;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+
+    document.body.style.background = colorString;
+}
